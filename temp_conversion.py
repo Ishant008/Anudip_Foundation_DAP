0b433d3400f2a10dae1d998c2f4a7a525540a98b
Ishant008 <tyagi8459800640@gmail.com>
2024-11-12 10:47:44 +0530

    Add files via upload

---
+++ temp_conversion.py
@@ -0,0 +1,8 @@
+#Input from the user (temp in celcius)
+celsius = float(input("Enter temperature in Celsius: "))
+
+# Convert Celsius to Fahrenheit
+fahrenheit = (celsius * 9/5) + 32
+
+# Display the result
+print(celsius,"°C is equal to ",fahrenheit)
